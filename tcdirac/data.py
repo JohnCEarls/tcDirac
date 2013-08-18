ef3737e2418fd82ac5345dfb0bf28c1dfd0301c1
John C. Earls <john.c.earls@gmail.com>
2013-08-18 18:08:45 +0000

    Created module to parse and manipulate data

---
+++ tcdirac/data.py
@@ -0,0 +1,118 @@
+from boto.dynamodb2.table import Table 
+from pandas import DataFrame
+import pandas
+import logging
+import os.path
+
+class SourceData:
+    """
+    A repository for data
+    """
+    def __init__(self):
+        logging.info("creating SourceData object")
+        self.source_dataframe = None
+        self.net_info = None
+    
+
+    def load_dataframe( self, data_frame_source='/scratch/sgeadmin/hddata/trimmed_dataframe.pandas' ):
+        logging.info("Loading existing expression dataframe[%s]" % data_frame_source)
+        try:
+            self.source_dataframe = pandas.read_pickle(data_frame_source)
+        except Exception as e:
+            logging.error("Error loading [%s]" % data_frame_source)
+            logging.error(str(e))
+            raise(e)
+    
+    def load_net_info(self,  table_name="net_info_table",source_id="c2.cp.biocarta.v4.0.symbols.gmt"):
+        self.net_info = NetworkInfo(table_name,source_id)
+
+    def getExpression(self, pathway_id, sample_ids=None):
+        """
+        Returns a dataframe containing only the genes in pathway
+        """
+        df = self.source_dataframe
+        ni = self.net_info
+        genes = ni.getGenes(pathway_id)
+        if not ni.isClean(pathway_id):
+            gset =  set(df.index)
+            genes = [g for g in genes if g in gset]
+            ni.updateGenes( pathway_id, genes )
+        if sample_ids is None:
+            #return all samples
+            return df.loc[genes,:]
+            
+class MetaInfo:
+    def __init__(self, meta_file):
+        self.metadata = pandas.io.parsers.read_table(meta_file)
+
+    def getSampleIDs(self, strain, allele=None):
+        md = self.metadata
+        if allele is None:
+            return md[md['strain'] == strain]['sample_id'].tolist()
+        else:
+            return md[(md['strain'] == strain) & (md['nominal_allele'] == allele)]['sample_id'].tolist()
+
+    def getStrains(self):
+        return self.metadata['strain'].unique().tolist()
+
+    def getNominalAlleles(self, strain=None):
+        md = self.metadata
+        if strain is None:
+            return md['allele_nominal'].unique().tolist()
+        else:
+            return md[md['strain'] == strain]['allele_nominal'].unique().tolist()
+
+
+class NetworkInfo:
+    """
+    Stores network information
+    """
+    def __init__(self, table_name="net_info_table",source_id="c2.cp.biocarta.v4.0.symbols.gmt"):
+        self.table = Table(table_name)
+        self.source_id = source_id
+        self.gene_map = {}
+        #clean refers to the genes being filtered to match the genes
+        #available in the expression file
+        self.gene_clean = {}
+
+    def getGenes(self, pathway_id, cache=True):
+        if pathway_id not in self.gene_map:
+            table = self.table
+            source_id = self.source_id
+            logging.info("Getting network info [%s.%s.%s]" % (table.table_name, source_id, pathway_id))
+            nit_item = table.get_item(src_id=source_id, pw_id=pathway_id)
+            self.gene_map[pathway_id] = nit_item['gene_ids'][6:].split('~:~')
+            self.gene_clean[pathway_id] = False
+        return self.gene_map[pathway_id]
+
+    def isClean(self, pathway_id):
+        return self.gene_clean[pathway_id]
+
+    def updateGenes(self, pathway_id, genes):
+        """
+        Updates gene list for pathway_id to genes(list)
+        """
+        self.gene_map[pathway_id] = genes
+        self.gene_clean[pathway_id] = True
+
+    def clearCache(self):
+        self.gene_map = {}
+        self.gene_clean = {}
+
+
+if __name__ == "__main__":
+    #getNetworkExpression( "c2.cp.biocarta.v4.0.symbols.gmt", "BIOCARTA_AKAPCENTROSOME_PATHWAY")
+    local_data_dir = '/scratch/sgeadmin/hddata/'
+    meta_file = os.path.join(local_data_dir, 'metadata.txt')
+    """
+    ni = NetworkInfo()
+    pathway_id = 'BIOCARTA_AKAPCENTROSOME_PATHWAY'
+    print ni.getGenes('BIOCARTA_AKAPCENTROSOME_PATHWAY')
+    sd = SourceData()
+    sd.load_dataframe()
+    sd.load_net_info()    
+    p_exp = sd.getExpression(pathway_id)
+    print p_exp"""
+    mi = MetaInfo(meta_file)
+    print mi.metadata
+
