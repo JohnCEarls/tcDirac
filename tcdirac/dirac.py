getRT was using the wrong shape parameter, so all comparisons were 0

tcdirac/dirac.py
@@ -1,8 +1,12 @@
+import static
+import debug
 import scipy.misc
 from pandas import DataFrame
 import itertools
 import numpy as np
-
+import os
+import os.path as op
+import logging
 
 def getSRT( df ):
     """
@@ -33,7 +37,9 @@ def getRT( df ):
     Returns a Series with rt
     
     """
-    nsamps = df.shape[0]
+    nsamps = df.shape[1]
+    if debug.DEBUG:
+        assert(nsamps == len(df.columns))
     return df.sum(1).apply(lambda x: 0 if 2*x < nsamps else 1)   
 
 def getRMS( srts, rt):