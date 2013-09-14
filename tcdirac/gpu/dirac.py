6818eab3d8605db1acb961f6acc3e7dab7a9862b
John C. Earls <john.c.earls@gmail.com>
2013-09-14 20:37:52 +0000

    New gpu structure

---
+++ tcdirac/gpu/dirac.py
@@ -0,0 +1,52 @@
+debug = True
+int32 = 4
+import kernels
+from pycuda.compiler import SourceModule
+
+def sampleRankTemplate( exp_gpu, gmap_gpu, nsamp, npairs, b1_size, b2_size ):
+    """
+    nsamp  is the columns dim (shape[1]) of exp_gpu
+    npairs is the length of gmap_gpu (shape[0])
+    """
+
+    if debug:
+        assert npairs%b1_size == 0
+        assert nsamp%b2_size == 0
+    
+    block = (b1_size, b2_size, 1)
+    grid = (npairs/b1_size, npairs/b2_size)
+
+    kernel_source = kernels.srt(nsamp)
+    mod = SourceModule(kernel_source)
+    func = mod.get_function('srtKernel')
+
+    srt_gpu = cuda.mem_alloc( nsamp*npairs*int32 )
+
+    func(exp_gpu, gmap_gpu, srt_gpu )
+    
+    return srt_gpu
+   
+
+def rankTemplate(  srt_gpu, sample_map_gpu, nsamples, neighbors, npairs, b1_size, b2_size):
+    """
+    srt_gpu is (npairs, nsamples)
+    sample_map_gpu is (neighbors, nsamples)
+    """
+
+    if debug:
+        assert npairs%b1_size == 0
+        assert nsamples%b2_size == 0
+    
+    block = (b1_size, b2_size, 1)
+    grid = (npairs/b1_size, nsamples/b2_size)
+    
+    kernel_source = kernels.rt( neighbors, nsamples )
+    mod = SourceModule(kernel_source)
+    func = mod.get_function('rtKernel')
+
+    rt_gpu = cuda.mem_alloc( nsamp*npairs*int32 )
+
+    func( srt_gpu, sample_map_gpu, rt_gpu)
+
+    return rt_gpu
+    
---
+++ tcdirac/gpu/kernels.py
@@ -0,0 +1,47 @@
+def srt(nsamples):
+    base = """
+    __global__ void srtKernel( float * d_expression, int *gene_map, int * srt){
+        int g_pair = blockIdx.x*blockDim.x + threadIdx.x;//(0,npairs-1)
+        int sample = blockIdx.y*blockDim.y + threadIdx.y;//(0,%i-1)
+       
+        int gm1 = gene_map[ 2*g_pair ];
+        int gm2 = gene_map[ 2*g_pair + 1 ];
+        
+        
+        float gene_1_exp = d_expression[gm1*%i + sample];
+
+        float gene_2_exp = d_expression[gm2*%i + sample];
+        srt[%i*g_pair + sample] = (int)(gene_1_exp < gene_2_exp);
+    }
+
+
+    """ % tuple([nsamples]*4)
+    return base
+
+
+
+def rt( neighbors, nsamples ):
+    base = """
+    __global__ void rtKernel( int * srt,  int * sample_map, int * rt){
+        int pair_gid = blockIdx.x*blockDim.x + threadIdx.x;
+        int samp_gid = blockIdx.y*blockDim.y + threadIdx.y;
+        int sm_off = samp_gid*%i;    
+        
+        int count = 0;
+        #pragma unroll
+        for(int i = 0; i<%i; i++){
+            count += srt[%i*pair_gid + sample_map[sm_off + i ]];
+        }
+        rt[ %i*pair_gid + samp_gid]   = %i < count;
+
+    }
+    """ % (neighbors, neighbors, nsamples, nsamples, neighbors/2)
+
+
+    return base
+
+
+if __name__ == "__main__":
+    print srt(10)
+
+    print rt(5, 10)
