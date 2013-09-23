f440029bf7a6710f571af4e27d43a4a5cdfea850
John C. Earls <john.c.earls@gmail.com>
2013-09-23 22:34:02 +0000

    added 2 class rms and rt kernels

--- tcdirac/gpu/kernels.py
+++ tcdirac/gpu/kernels.py
@@ -40,6 +40,26 @@ def rt( neighbors, nsamples ):
 
     return base
 
+def rtBin( nsamples ):
+    base = """
+    __global__ void rtKernel( int * srt, int * sample_map, int * rt){
+        __shared__ int sm[%i];
+        int pair_gid = blockIdx.x*blockDim.x + threadIdx.x;
+        for(int i=threadIdx.x; i<%i; i+=blockDim.x){
+            sm[i] = sample_map[i];
+        }
+        __syncthreads();
+        int count = 0;
+        int ss_size = 0;
+        #pragma unroll
+        for(int i=0;i<%i;i++){
+            count += srt[%i*pair_gid + i] & sm[i];
+            ss_size += sm[i];
+        }
+        rt[pair_gid] = ss_size/2 < count;
+    } """ % (nsamples,nsamples,nsamples,nsamples)
+
+    return base
 def rms( nsamples, nnets ):
     base = """
     __global__ void rmsKernel( int * rt, int * srt,  int * net_map, float * rms){
@@ -60,6 +80,26 @@ def rms( nsamples, nnets ):
 
     return base
 
+def rmsBin( nsamples, nnets ):
+    base = """
+    __global__ void rmsKernel( int * rt, int * srt,  int * net_map, float * rms){
+        int net =  blockIdx.x*blockDim.x + threadIdx.x;
+        int sample = blockIdx.y*blockDim.y + threadIdx.y;
+        int counter = 0;
+        int nm_start = net_map[net];
+        int nm_end = net_map[net+1];
+
+        for( int i=nm_start; i< nm_end; i++){
+            counter += rt[i] == srt[%i * i + sample];
+        }
+        if (nm_end-nm_start > 0){//counter == 0 if net is in the buffer zone
+            rms[net*%i + sample ] = (float)counter/(float)(nm_end - nm_start);
+        }
+
+    } """ % (nsamples, nsamples)
+
+    return base
+
 
 if __name__ == "__main__":
     print srt(10)
@@ -67,3 +107,6 @@ if __name__ == "__main__":
     print rt(5, 10)
 
     print rms(10, 10)
+    print rtBin(1000)
+
+    print rmsBin(1000,200)
