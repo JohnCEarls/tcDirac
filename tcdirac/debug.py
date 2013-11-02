790c30b71a81b29e1f001a1429c273ad8bd706c4
John C. Earls <john.c.earls@gmail.com>
2013-11-02 19:25:45 +0000

    Refactoring.  Moving loader to gpu module and out of gpu-cluster

--- tcdirac/debug.py
+++ tcdirac/debug.py
@@ -1,2 +1,63 @@
 DEBUG = True
 debug_dir='/scratch/sgeadmin/debug'
+import time
+import logging
+import os
+import os.path
+class TimeTracker:
+    def __init__(self):
+        self._wait_tick = time.time()
+        self._work_tick = time.time()
+        self._waiting = 0.0
+        self._working = 0.0
+
+    def start_work(self):
+        self._work_tick= time.time()
+
+    def end_work(self):
+        self._working += time.time() - self._work_tick
+        self._work_tick = time.time()
+
+
+    def start_wait(self):
+        self._wait_tick= time.time()
+
+    def end_wait(self):
+        self._waiting += time.time() - self._wait_tick
+        self._wait_tick = time.time()
+
+    def print_stats(self):
+        print
+        print "Waiting Time:", self._waiting
+        print "Working Time:", self._working
+        print "working/waiting", self._working/self._waiting
+        print
+
+def initLogging(lname="tcdirac",level=logging.DEBUG, st_out=True):
+    try:
+        rank = MPI.COMM_WORLD.rank
+    except NameError:
+        rank = 0
+    logdir = '/scratch/sgeadmin/logs/'
+    if rank == 0:
+        if not os.path.exists(logdir):
+            os.makedirs(logdir)
+    else:
+        while not os.path.exists(logdir):
+            time.sleep(1)
+
+    log_format = '%(asctime)s - %(name)s rank['+str( rank )+']- %(levelname)s - %(message)s'
+    logging.basicConfig(filename=os.path.join(logdir,lname), level=level, format=log_format)
+    logging.info("Starting")
+    if st_out:
+        sendLogSO()
+    
+
+def sendLogSO():
+    root = logging.getLogger()
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    root.addHandler(ch)
+
